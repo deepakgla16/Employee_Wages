0e1f9cb0c3351a6ebca5a9afa34da8c5670229d7
deepakgla16 <deepaksharma972079@gmail.com>
2025-03-24 10:35:08 +0530

    Emm wages according to days hours and daily wages in array

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOURS = 20;
+ const IS_FULL_TIME = 2;
+ const PART_TIME_HOURS = 4;
+ const FULL_TIME_HOURS = 8;
+ const WAGE_PER_HOUR = 20;
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -14,18 +14,27 @@ function getWorkingHours(empCheck) {
             return 0;    
     }
 }
+function calDailyWages(emphrs) {
+    return emphrs*WAGE_PER_HOUR;
+}
 
-const maxhoursInMonth=100;
-const NumOfWorkingDays=10
+const MAX_HRS_IN_MONTH=160;
+const NUM_OF_WORKING_DAYS=20;
+
 let totalEmpHrs=0;
 let totalWorkingDays=0;
 
+let empDailyWage=new Array();
 
-while (totalEmpHrs <= maxhoursInMonth && totalWorkingDays  <= NumOfWorkingDays) {
+while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<=NUM_OF_WORKING_DAYS){
     totalWorkingDays++;
-    let empCheck=Math.floor(Math.random()*10)%3;    
-    totalEmpHrs+=getWorkingHours(empCheck);
+    let empCheck=Math.floor(Math.random()*10)%3
+    let empHrs=getWorkingHours(empCheck);
+        totalEmpHrs+=empHrs
+        empDailyWage.push(calDailyWages(empHrs))
 }
 
-let empWage=totalEmpHrs*WAGE_PER_HOURS;
-console.log("Total Days:"+totalWorkingDays+" Total hrs:"+totalEmpHrs+" Emp Wage:"+empWage);
+
+
+let empWage=calDailyWages(totalEmpHrs);
+console.log("Total days:"+totalWorkingDays+" total hrs:"+totalEmpHrs+"Emp Wage:"+empWage);
